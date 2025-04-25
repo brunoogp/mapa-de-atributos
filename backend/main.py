e96f194acdffde55d087c41c101c945a643e04df
brunoogp <147428822+brunoogp@users.noreply.github.com>
2025-04-25 16:02:00 -0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -7,10 +7,7 @@ app.include_router(router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://mapa-de-atributos.vercel.app",  # ✅ frontend hospedado
-        "http://localhost:3000",  # ✅ útil para testes locais
-    ],
+    allow_origins=["https://mapa-de-atributos.vercel.app"],  # 👈 Aqui!
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
