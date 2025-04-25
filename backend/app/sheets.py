5d9a0fe6967209a8113b3c31543cacc3a1e5bf82
brunoogp <147428822+brunoogp@users.noreply.github.com>
2025-04-25 11:55:27 -0300

    Update sheets.py

--- backend/app/sheets.py
+++ backend/app/sheets.py
@@ -1,18 +1,21 @@
 # backend/app/sheets.py
 
 import os
+import json
 from google.oauth2.service_account import Credentials
 from googleapiclient.discovery import build
 
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
-SPREADSHEET_ID = "1--YM0Av6A2cHa5NU8JfYbFn1I0VHov5mRbQhIzKEsvE"  # substitua pelo seu se mudar
+SPREADSHEET_ID = "1--YM0Av6A2cHa5NU8JfYbFn1I0VHov5mRbQhIzKEsvE"  # substitua se mudar
 SHEET_NAME = "Página1"  # nome da aba
-SERVICE_ACCOUNT_FILE = "credentials.json"
+
+# Carrega o JSON de credenciais da variável de ambiente
+SERVICE_ACCOUNT_INFO = json.loads(os.environ["GOOGLE_CREDENTIALS_JSON"])
 
 def adicionar_linha(dados: list):
     print("DEBUG - Dados recebidos:", dados)
 
-    creds = Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+    creds = Credentials.from_service_account_info(SERVICE_ACCOUNT_INFO, scopes=SCOPES)
     service = build("sheets", "v4", credentials=creds)
     sheet = service.spreadsheets()
 
