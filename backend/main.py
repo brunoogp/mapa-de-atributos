12d6da22434daa881b2b7d8b83b923ed8b75222a
brunoogp <147428822+brunoogp@users.noreply.github.com>
2025-04-29 17:16:06 -0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -6,10 +6,18 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://mapa-de-atributos.vercel.app"],  # 🔥 use somente o domínio da Vercel
+    allow_origins=["https://mapa-de-atributos.vercel.app"],
     allow_credentials=True,
-    allow_methods=["*"],  # ou ["POST"] se quiser restringir
+    allow_methods=["*"],
     allow_headers=["*"]
 )
 
 app.include_router(router)
+
+# 👇 Adicione isso ao final
+if __name__ == "__main__":
+    import os
+    import uvicorn
+
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
